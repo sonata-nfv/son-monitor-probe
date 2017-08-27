d60118f68ebc6dcc774bbae41c739bc06eef6865
Panagiotis Karkazis <pkarkazis@synelixis.com>
2017-08-27 21:08:09 +0300

    bug fix

--- sp_mon/ContData.py
+++ sp_mon/ContData.py
@@ -62,7 +62,7 @@ class cntdt:
         for cnt in self.data:
             if len(cnt['stats']) == 0:
                 continue
-            dt2sent = True
+            self.dt2sent = True
             cnt_created += "cnt_created{id=\""+cnt['id']+"\",image_name=\""+cnt['image_name']+"\",image=\""+cnt['image']+"\",name=\""+cnt['name'][0]+"\"}" +str(cnt['created']) + timestamp + '\n'
             cnt_status += "cnt_status{id=\""+cnt['id']+"\",image_name=\""+cnt['image_name']+"\",image=\""+cnt['image']+"\",name=\""+cnt['name'][0]+"\"}" +str(self.checkNone(cnt['status'])) + timestamp + '\n'
             cnt_cpu_perc += "cnt_cpu_perc{id=\""+cnt['id']+"\",image_name=\""+cnt['image_name']+"\",image=\""+cnt['image']+"\",name=\""+cnt['name'][0]+"\"}"+str(self.checkNone(cnt['stats']['cpu_perc'])) + timestamp + '\n'
@@ -75,7 +75,7 @@ class cntdt:
             cnt_block_ou_MB += "cnt_block_ou_MB{id=\""+cnt['id']+"\",image_name=\""+cnt['image_name']+"\",image=\""+cnt['image']+"\",name=\""+cnt['name'][0]+"\"}" +str(self.checkNone(cnt['stats']['block_ou_MB'])) + timestamp + '\n'
 
         data = cnt_created + cnt_cpu_perc +cnt_mem_perc + cnt_mem_usage_MB + cnt_mem_limit_MB + cnt_net_rx_MB + cnt_net_tx_MB + cnt_block_in_MB + cnt_block_ou_MB + cnt_status
-        if dt2sent:
+        if self.dt2sent:
             return data
         else:
             None
